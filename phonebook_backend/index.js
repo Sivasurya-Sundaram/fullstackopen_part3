014ecaafceb868af9084a649b9a8f9ef69b1dfff
Sivasurya Sundaram <gamerganapathi@live.com>
2023-08-12 13:01:11 +0530

    exercise 3.7-3.8

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+morgan.token("requestBody", (request, response) => {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
+  }
+});
 app.use(express.json());
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :requestBody"));
 
 let contacts = [
   {
